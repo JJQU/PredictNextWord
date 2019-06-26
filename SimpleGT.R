b8ef27af16bf5541781bbb3e65a45e1549db9b31
JJQU <37889191+JJQU@users.noreply.github.com>
2019-06-26 17:44:59 -0400

    Update SimpleGT.R

--- SimpleGT.R
+++ SimpleGT.R
@@ -6,19 +6,7 @@
 ## by Willian A. Gale
 ## https://faculty.cs.byu.edu/~ringger/CS479/papers/Gale-SimpleGoodTuring.pdf
 
-## Simple Good-Turing method:
-##   1. Use Turing estimate for small r
-##   2. Use Linear Good-Turing estimate for large r
-##   3. Switch to LGT when r(T) - r(LGT) > 1.65*sd
-##   4. Once switched to LGT, keep using LGT
-##
-## simpleGT takes a data table of frequencies r and frequencies of frequencies 
-## Nr and calculated the smoothed frequencies by the Simple Good Turing method.
-## freqhist = imput, a data table of r (freq) and Nr (NFreq)
-## rrstar = output, a data table of r (freq) and rstar (freqGT)
-## where freq = original freq, freqGT = freq smoothed by simple GT method.
-## the first row is the pair of 0 freq = (0, freqGT_at_0)
-##
+
 simpleGT <- function(freqhist) {
   
   ## nrzest: Averaging transformation
@@ -105,26 +93,3 @@ simpleGT <- function(freqhist) {
   return(rrstar)
 }
 
-## Test cases
-## from Gale-SimpleGoodTuring.pdf referenced above.
-
-# prosody.r <- c(1,2,3,4,5,6,7,8,9,10,12,14,15,16,17,19,20,21,23,24,
-#                25,26,27,28,31,32,33,34,36,41,43,45,46,47,50,71,84,101,105,121,
-#                124,146,162,193,199,224,226,254,257,339,421,456,481,483,1140,
-#                1256,1322,1530,2131,2395,6925,7846)
-# prosody.nr <- c(120,40,24,13,15,5,11,2,2,1,3,2,1,1,3,1,3,2,3,3,
-#                 3,2,2,1,2,2,1,2,2,3,1,3,1,1,1,1,1,1,1,1,
-#                 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1)
-# 
-# prosody <- cbind(prosody.r, prosody.nr)
-# prosodyRstar <- simpleGT(prosody)
-# 
-# chinesePlurals.r <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-#                       21,22,23,24,25,26,27,28,29,31,33,39,41,46,47,
-#                       50,52,53,55,57,60,74,84,108,109,177,400,1918)
-# 
-# chinesePlurals.nr <- c(268,112,70,41,24,14,15,14,8,11,9,6,6,3,7,9,4,4,8,2,
-#                        4,2,2,3,4,4,4,1,1,2,1,3,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1)
-# 
-# chinesePlurals <- cbind(chinesePlurals.r, chinesePlurals.nr)
-# chinesePluralsRstar <- simpleGT(chinesePlurals)
