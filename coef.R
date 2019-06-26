ed1078afaecb6c3b020d798075b38df4f8831870
JJQU <37889191+JJQU@users.noreply.github.com>
2019-06-26 17:50:15 -0400

    Add files via upload

---
+++ coef.R
@@ -0,0 +1,8 @@
+
+## Simplex search for linear coefficients
+## see Nelder-Mead algorithm : http://www.scholarpedia.org/article/Nelder-Mead_algorithm
+## Initial simplex:
+## c1    c2    c3
+## 0.0   0.0   1.0
+## 0.0   0.3   0.7
+## 0.3   0.0   0.7
