782ff15296161ddc30824cac3a480740c495ffe7
JJQU <37889191+JJQU@users.noreply.github.com>
2019-06-26 17:24:33 -0400

    Update server.R

--- Shiny App/server.R
+++ Shiny App/server.R
@@ -8,54 +8,15 @@ suppressWarnings(library(dplyr))
 suppressWarnings(library(ngram))
 
 
-load(file = "~/Shiny/PredictNextWord1/model2.rda")
-source('~/Shiny/PredictNextWord1/NgramModeling.R')
-source('~/Shiny/PredictNextWord1/Preprocess.R')
+#load(file = "~/Shiny/PredictNextWord1/model2.rda")
 
+source('./Prediction.R')
 
 shinyServer(function(input, output) {
   
-  predict <- function(inputStr) {
-    ## Check for an empty string
-    if (inputStr == "") {
-      return("<UNK>")
-    }
-    inputCorpus <- SimpleCorpus(VectorSource(inputStr))
-    inputCorpus <- preprocessData(inputCorpus)
-    
-    cleanStr <- concatenate(inputCorpus$content)[[1]]
-    if (cleanStr == " ") {
-      return("<UNK>")
-    }
-    
-    inputWords <- str_split(cleanStr, boundary("word"))[[1]]
-    
-    if (length(inputWords) > 1) {
-      testWords <- tail(inputWords, 2)
-    } else if (length(inputWords) > 0) {
-      testWords <- c("<UNK>", tail(inputWords, 1))
-    } else {
-      return("<UNK>")
-    }
-    
-    predictions <- trimodel(testWords, coef2, 
-                            ugramPzero.f8, 
-                            bgramPzero.f2.f8, 
-                            tgramPzero.f2.f8, 1)
-    
-    cnames <- colnames(predictions)
-    if ("word3" %in% cnames) {
-      nextWords <- predictions$word3
-    } else if ("word2" %in% cnames) {
-      nextWords <- predictions$word2
-    } else {
-      nextWords <- predictions$word1
-    }
-    return(nextWords)
-  }
   
   output$NextWord <- renderPrint({
-    result <- predict(input$inputText)
+    result <- predict2(input$inputText)
     result});
   
   output$inputWords <- renderText({
